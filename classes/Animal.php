07a0317d288e64d85eb55836ade69ecd413566ea
MartinKokesCz <you@example.com>
2021-11-25 12:12:17 +0100

    animal class

---
+++ classes/Animal.php
@@ -0,0 +1,17 @@
+<?php
+
+abstract class Animal
+{
+    private string $name;
+    private int $legs;
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getLegs()
+    {
+        return $this->legs;
+    }
+}
